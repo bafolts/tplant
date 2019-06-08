7d30860df35ad366e4ebdd9a5dd582fcf003f3b8
Brian Folts <brian.folts@gmail.com>
2019-06-07 19:15:24 -0700

    Increase version to 2.3.1

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ import { tplant } from './tplant';
 const AVAILABLE_PLANTUML_EXTENSIONS: string[] = ['svg', 'png', 'txt'];
 
 commander
-    .version('2.3.0')
+    .version('2.3.1')
     .option('-i, --input <path>', 'Define the path of the Typescript file')
     .option('-o, --output <path>', 'Define the path of the output file. If not defined, it\'ll output on the STDOUT')
     .option(
