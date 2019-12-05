01f86f2fc0b61c7c15cb88f498b45819d95d9cb1
Jeff Tian <jie.tian@unicareer.com>
2019-12-05 16:34:44 +0800

    feat: save file with utf-8 encoding to support CJK characters

---
+++ test/CJK/CJK.ts
@@ -0,0 +1,10 @@
+namespace CJK {
+    /**
+     * ChineseCharacters
+     *
+     * Some classes might contains some Chinese characters
+     */
+    class ChineseCharacters {
+        public 你好: string = '';
+    }
+}
