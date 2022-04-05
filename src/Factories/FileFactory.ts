73f89141f408aeb40b1c610b1a26a0210d0401f6
Adrian Burlacu <adrian.burlacu@live.com>
2022-04-05 17:23:19 +0300

    Try fixing the file names.

--- src/Factories/FileFactory.ts
+++ src/Factories/FileFactory.ts
@@ -4,7 +4,7 @@ import { ComponentFactory } from './ComponentFactory';
 
 export namespace FileFactory {
     export function create(sourceFile: ts.Node, checker: ts.TypeChecker): File {
-        const file: File = new File();
+        const file: File = new File(sourceFile.name);
         file.parts = ComponentFactory.create(sourceFile, checker);
 
         return file;
