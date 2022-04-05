4b5a0f6360b69882ea0f6c92f98f8bcd86a258ff
Adrian Burlacu <adrian.burlacu@live.com>
2022-04-05 17:30:14 +0300

    Fix filename bug :)

--- src/Factories/FileFactory.ts
+++ src/Factories/FileFactory.ts
@@ -3,8 +3,8 @@ import { File } from '../Components/File';
 import { ComponentFactory } from './ComponentFactory';
 
 export namespace FileFactory {
-    export function create(sourceFile: ts.Node, checker: ts.TypeChecker): File {
-        const file: File = new File(sourceFile.getName());
+    export function create(sourceFile: ts.SourceFile, checker: ts.TypeChecker): File {
+        const file: File = new File(sourceFile.fileName);
         file.parts = ComponentFactory.create(sourceFile, checker);
 
         return file;
