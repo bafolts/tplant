e0dda8069be3817c3ad39f37048e2177500e9fd8
Adrian Burlacu <63519230+adrian-burlacu-software@users.noreply.github.com>
2022-04-06 11:24:07 +0300

    Revert "Populate Components.File.Name"
    This reverts commit 0c23cc289701b7b8a34849eed118025f4c6ec5a8.

--- src/Components/File.ts
+++ src/Components/File.ts
@@ -1,10 +1,11 @@
 import { ComponentKind } from '../Models/ComponentKind';
-import { ComponentComposite, IComponentComposite } from '../Models/IComponentComposite';
+import { IComponentComposite } from '../Models/IComponentComposite';
 
 /**
  * Represents the metadata for a file containing typescript
  */
-export class File extends ComponentComposite {
+export class File implements IComponentComposite {
     public readonly componentKind: ComponentKind = ComponentKind.FILE;
+    public readonly name: string = '';
     public parts: IComponentComposite[] = [];
 }
--- src/Factories/FileFactory.ts
+++ src/Factories/FileFactory.ts
@@ -3,8 +3,8 @@ import { File } from '../Components/File';
 import { ComponentFactory } from './ComponentFactory';
 
 export namespace FileFactory {
-    export function create(sourceFile: ts.SourceFile, checker: ts.TypeChecker): File {
-        const file: File = new File(sourceFile.fileName);
+    export function create(sourceFile: ts.Node, checker: ts.TypeChecker): File {
+        const file: File = new File();
         file.parts = ComponentFactory.create(sourceFile, checker);
 
         return file;
