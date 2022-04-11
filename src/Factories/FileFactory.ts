d48d05bf03ee9a5fc6b0df6f7a28b92c0c93e2d3
Adrian Burlacu <adrian.burlacu@live.com>
2022-04-11 10:25:20 +0300

    Merge in File.fileName fix.

--- src/Factories/FileFactory.ts
+++ src/Factories/FileFactory.ts
@@ -3,8 +3,8 @@ import { File } from '../Components/File';
 import { ComponentFactory } from './ComponentFactory';
 
 export namespace FileFactory {
-    export function create(fileName: string, sourceFile: ts.Node, checker: ts.TypeChecker): File {
-        const file: File = new File();
+    export function create(fileName: string, sourceFile: ts.SourceFile, checker: ts.TypeChecker): File {
+        const file: File = new File(sourceFile.fileName);
         file.parts = ComponentFactory.create(fileName, sourceFile, checker);
 
         return file;
